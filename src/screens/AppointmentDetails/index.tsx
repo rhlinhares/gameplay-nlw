17b47a6cdcb0ed900fbd62b8b7646c1756703fa1
Raphael Hubener Linhares <raphael.linhares@gmail.com>
2021-07-02 18:31:40 -0300

    Aula 03

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Fontisto } from '@expo/vector-icons';
+
+import { ImageBackground, View, Text, FlatList } from 'react-native';
+import { Background } from '../../components/Background';
+import { Header } from '../../components/Header';
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
+import { ListDivider } from '../../components/ListDivider';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import BannerImg from '../../assets/banner.png';
+
+export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Rodrigo',
+      avatar_url: 'https://github.com/rodrigorgtic.png',
+      status: 'online',
+    },
+    {
+      id: '2',
+      username: 'Rodrigo',
+      avatar_url: 'https://github.com/rodrigorgtic.png',
+      status: 'offline',
+    },
+  ];
+
+  return (
+    <Background>
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto name="share" size={24} color={theme.colors.primary} />
+          </BorderlessButton>
+        }
+      />
+      <ImageBackground source={BannerImg} style={styles.banner}>
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>Lendários</Text>
+
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da m10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader title="Jogadores" subtitle="Total 3" />
+      <FlatList
+        data={members}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Member data={item} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
+
+      <View style={styles.footer}>
+        <ButtonIcon title="Entrar na partida" />
+      </View>
+    </Background>
+  );
+}
---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, FlatList, Text } from 'react-native';
+
+import { Guild, GuildProps } from '../../components/Guild';
+import { ListDivider } from '../../components/ListDivider';
+import { styles } from './styles';
+
+type Props = {
+  handleGuildSelect: (guild: GuildProps) => void;
+};
+
+export function Guilds({ handleGuildSelect }: Props) {
+  const guilds = [
+    {
+      id: '1',
+      name: 'Lendários',
+      icon: null,
+      owner: true,
+    },
+    {
+      id: '2',
+      name: 'Únicos',
+      icon: null,
+      owner: false,
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.guilds}
+        data={guilds}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Guild data={item} onPress={() => handleGuildSelect(item)} />
+        )}
+        ItemSeparatorComponent={() => <ListDivider />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
